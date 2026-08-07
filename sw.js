094b90b0f0c2c60d95729d0114b4a1247150ddc1
Zeevek <153991856+Zeevek@users.noreply.github.com>
2026-08-07 12:35:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
        satureraient le quota du navigateur ;
      - les requêtes non-GET.
 */
-const CACHE = "medaillier-v21";
+const CACHE = "medaillier-v22";
 const FICHIERS = [
   "./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png",
